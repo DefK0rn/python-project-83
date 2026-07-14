f1ba7e3a98760d713c739c01f586fb37976f136c
DefK0rn <defkorn@yandex.ru>
2026-07-14 19:00:05 +0300

    Add badge GitHub action

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/DefK0rn/python-project-83/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/DefK0rn/python-project-83/actions)
+
+### GitHub Action workflow
+[![page_analyzer CI](https://github.com/DefK0rn/python-project-83/actions/workflows/pyci.yml/badge.svg)](https://github.com/DefK0rn/python-project-83/actions/workflows/pyci.yml)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,5 @@
 import page_analyzer
+from page_analyzer import app
 
 
 def test_app_at_package_level():
@@ -6,3 +7,36 @@ def test_app_at_package_level():
     assert hasattr(page_analyzer, 'app')
     
     assert 'app' in page_analyzer.__all__
+
+
+def test_index_page_status():
+    with app.test_client() as client:
+        response = client.get('/')
+        assert response.status_code == 200
+
+
+def test_index_page_content():
+    with app.test_client() as client:
+        response = client.get('/')
+        
+        html = response.data.decode('utf-8')
+
+        # Проверяем главный заголовок и описание
+        assert '<title>Анализатор страниц</title>' in html
+        assert 'Бесплатно проверяйте сайты на SEO-пригодность' in html
+        
+        # Проверяем, что форма отправляет POST-запрос
+        assert 'action="/urls"' in html
+        assert 'method="post"' in html
+        
+        # Проверяем наличие поля ввода для URL и кнопки отправки
+        assert 'type="url"' in html
+        assert 'name="url"' in html
+        assert 'placeholder="https://www.example.com"' in html
+
+        # Проверяем навигацию
+        assert 'href="/urls"' in html
+        assert 'Сайты' in html
+        
+        # Проверяем гиперссылку в подвале
+        assert 'href="https://hexlet.io"' in html
